2a7f5afb4a5cf991746c377d54a5bb51fb725d08
Izabella Alves <alves.s.izabella@gmail.com>
2026-03-17 19:23:05 -0300

    Update PyInvest.py
    PyInvesti.py - Entrega de projeto.

--- PyInvest.py
+++ PyInvest.py
@@ -1,101 +1,102 @@
-
 import math
 import random
 import datetime
-import statistics
+import statistics as st
 import locale
 
-locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
+locale.setlocale(locale.LC_ALL,'pt_BR.UTF-8')
 
-# ENTRADAS
+#entrada 
+
 capital = float(input('Capital inicial: '))
-aporte = float(input('Aporte mensal: '))
-meses = int(input('Prazo (meses): '))
-cdi_anual = float(input('CDI anual %: ')) / 100
-perc_cdb = float(input('Percentual do CDI - CDB (%): ')) / 100
-per_lci = float(input('Percentual do CDI - LCI (%): ')) / 100
-taxa_fii = float(input('Rentabilidade do FII (%): ')) / 100
-meta = float(input('Meta financeira (R$): '))
+aporte = float(input('Aporte inicial: '))
+meses = float(input('Prazo(meses): '))
+cdi_anual = float(input('CDI anual (%): '))/100
+perc_cdb = float(input('Percentua do CDI - CDB (%): '))/100
+perc_lci = float(input('Percentual do CDI - LCI (%): '))/100
+taxa_fii = float(input('Rentabilidade do FII (%): '))/100
+meta = float(input('Meta finaceira (R$): '))
 
-# conversão CDI
-cdi_mensal = math.pow((1 + cdi_anual), 1/12) - 1
+#conversão do CDI
+cdi_mensal = math.pow((1+cdi_anual), 1/12) - 1
 
-# total investido
+#total investido
 total_investido = capital + (aporte * meses)
 
-# CDB
+#CDB
 taxa_cdb = cdi_mensal * perc_cdb
-montante_cdb = (capital * math.pow((1 + taxa_cdb), meses)) + (aporte * meses)
+montante_cdb = (capital * math.pow((1+taxa_cdb), meses))+(aporte * meses)
 lucro_cdb = montante_cdb - total_investido
 montante_cdb_liquido = total_investido + (lucro_cdb * 0.85)
 
-# LCI/LCA
-taxa_lci = cdi_mensal * per_lci
-montante_lci = (capital * math.pow((1 + taxa_lci), meses)) + (aporte * meses)
+#LCI/LCA
+taxa_lci = cdi_mensal * perc_lci
+montante_lci = (capital * math.pow((1+taxa_lci), meses)) +(aporte * meses)
 
-# poupança
-taxa_poupanca = 0.005
-montante_poupanca = (capital * math.pow((1 + taxa_poupanca), meses)) + (aporte * meses)
+#Poupança
+taxa_poupanca = 0.005 
+montante_poupanca = (capital * math.pow((1+taxa_poupanca), meses)) + (aporte * meses)
 
-# estatística FII
-fii1 = (capital * math.pow((1 + taxa_fii), meses)) + (aporte * meses)
-fii1 = fii1 * (1 + random.uniform(-0.03, 0.03))
+#FII
+montante_fii = (capital * math.pow((1+taxa_fii), meses)) + (aporte * meses) 
+vari1 = random.uniform(-0.003, 0.003)
+vari2 = random.uniform(-0.003, 0.003)
+vari3 = random.uniform(-0.003, 0.003)
+vari4 = random.uniform(-0.003, 0.003)
+vari5 = random.uniform(-0.003, 0.003)
 
-fii2 = (capital * math.pow((1 + taxa_fii), meses)) + (aporte * meses)
-fii2 = fii2 * (1 + random.uniform(-0.03, 0.03))
+simu1 = montante_fii + (montante_fii * vari1)
+simu2 = montante_fii + (montante_fii * vari2)
+simu3 = montante_fii + (montante_fii * vari3)
+simu4 = montante_fii + (montante_fii * vari4)
+simu5 = montante_fii + (montante_fii * vari5)
 
-fii3 = (capital * math.pow((1 + taxa_fii), meses)) + (aporte * meses)
-fii3 = fii3 * (1 + random.uniform(-0.03, 0.03))
+media_fii = st.mean((simu1, simu2, simu3, simu4, simu5))
+mediana_fii = st.median((simu1, simu2, simu3, simu4, simu5))
+desvio_fii = st.stdev((simu1, simu2, simu3, simu4, simu5))
 
-fii4 = (capital * math.pow((1 + taxa_fii), meses)) + (aporte * meses)
-fii4 = fii4 * (1 + random.uniform(-0.03, 0.03))
+#GRAFICO
+blocos_cdb = int(montante_cdb_liquido )/ 1000
+blocos_lci = int(montante_lci )/ 1000
+blocos_poupamca = int(montante_poupanca )/ 1000
+blocos_fii = int(montante_fii )/ 1000
 
-fii5 = (capital * math.pow((1 + taxa_fii), meses)) + (aporte * meses)
-fii5 = fii5 * (1 + random.uniform(-0.03, 0.03))
+#data
+data_inicial = datetime.date.today()
+data_final = data_inicial + datetime.timedelta(days= meses * 30)
 
-# statistics
-media_fii = statistics.mean([fii1, fii2, fii3, fii4, fii5])
-mediana_fii = statistics.median([fii1, fii2, fii3, fii4, fii5])
-desvio_fii = statistics.stdev([fii1, fii2, fii3, fii4, fii5])
+#Formatar datas
+data_simulacao = data_inicial.strftime('%d/%m/%Y')
+data_resgate_fmt = data_final.strftime('%d/%m/%Y')
 
-# datetime
-data_simulacao = datetime.datetime.now()
-dias = meses * 30
-data_resgate = data_simulacao + datetime.timedelta(days=dias)
+#meta
+meta_atingida = media_fii >=meta
 
-# meta
-meta_atingida = media_fii >= meta
-
-# gráficos
-grafico_cdb = "█" * int(montante_cdb_liquido / 1000)
-grafico_lci = "█" * int(montante_lci / 1000)
-grafico_poupanca = "█" * int(montante_poupanca / 1000)
-grafico_fii = "█" * int(media_fii / 1000)
-
-print("\nRELATÓRIO DE INVESTIMENTOS\n")
-
-print("Data da simulação:", data_simulacao.strftime("%d/%m/%Y"))
-print("Data estimada de resgate:", data_resgate.strftime("%d/%m/%Y"))
-
-print("\nTotal investido:", locale.currency(total_investido, grouping=True))
-
-print("\nValores finais")
-
-print("CDB:", locale.currency(montante_cdb_liquido, grouping=True))
-print("LCI/LCA:", locale.currency(montante_lci, grouping=True))
-print("Poupança:", locale.currency(montante_poupanca, grouping=True))
-print("FII (média):", locale.currency(media_fii, grouping=True))
-
-print("\nEstatísticas do FII")
-print("Média:", locale.currency(media_fii, grouping=True))
-print("Mediana:", locale.currency(mediana_fii, grouping=True))
-print("Desvio padrão:", locale.currency(desvio_fii, grouping=True))
-
-print("\nMeta atingida:", meta_atingida)
-
-print("\nGráficos")
-
-print("CDB      ", grafico_cdb)
-print("LCI/LCA  ", grafico_lci)
-print("Poupança ", grafico_poupanca)
-print("FII      ", grafico_fii)
+print('-' * 40)
+print(" PyInvest Simulador de Investimentos")
+print('=' * 40)
+print(f'Data da simulacao:{data_inicial.strftime("%d/%m/%Y")}')
+print(f'Data de resgaste:{data_final.strftime("%d/%m/%Y")}')
+print('=' * 40)
+print('       ')
+print(f'Total investido:{locale.currency(total_investido, grouping=True)}')
+print('       ')
+print('--- RESULTADOS FINANCEIROS ---')
+print(f'CDB: {locale.currency(montante_cdb_liquido, grouping=True)}')
+print(f'{'█'* int(montante_cdb_liquido / 1000)}')
+print('       ')
+print(f'LCI:{locale.currency(montante_lci, grouping=True)}')
+print(f'{'█'* int(montante_lci / 1000)}')
+print('       ')
+print(f'Poupanca:{locale.currency(montante_poupanca, grouping=True)}')
+print(f'{'█'* int(montante_poupanca / 1000)}')
+print('       ')
+print(f'FII(media:{locale.currency(media_fii, grouping=True)}')
+print(f'{'█'* int(media_fii / 1000)}')
+print('       ')
+print('--- ESTATISTICAS FII ---')
+print(f'Mediana:{locale.currency(mediana_fii, grouping=True)}')
+print(f'Desvio padrao:{locale.currency(desvio_fii, grouping=True)}')
+print('       ')
+print(f'Meta atingida?:{media_fii >= meta}')
+print('='*40)
